7f3321f9dd868e54722e67a460449bb672cd8d8d
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2024-03-26 00:08:24 +0530

    Create Question List.java

---
+++ Java-Taking-input/Question List.java
@@ -0,0 +1,5 @@
+Q1 - Input name, roll number and field of interest from user and print in the format below :Name: xyz, Roll number: xyz, Field of interest: xyz.
+Q2 - Input two different string and print them in same line.
+Q3 - If the marks of Robert in three subjects are entered through keyboard (each out of 100), write a program to calculate his total marks and percentage marks.
+Q4 - Given two numbers, return their sum in the following format: Int t representing number of test cases T lines of Two integers representing the numbers to be added.
+Q5 - Given few lines of input(number of lines unknown) where each line has two strings, concatenate the strings.
