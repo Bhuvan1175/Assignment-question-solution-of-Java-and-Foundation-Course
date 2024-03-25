36b5f850077a010464fbd55e7578c7a1b4174716
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2024-03-25 23:48:07 +0530

    Create Q2 - Print the ASCII value of character ‘U’,(EASY).java

---
+++ Java-variable-and-data-type/Q2 - Print the ASCII value of character ‘U’,(EASY).java
@@ -0,0 +1,8 @@
+//Q2 - Print the ASCII value of character ‘U’.(EASY)
+public class secondqQueASCII {
+    public static void main(String[] args) {
+        char ch = 'U';
+        int ascii = (int) ch;
+        System.out.println("The ASCII Value of " + ch + " is : " + ascii);
+    }
+}
