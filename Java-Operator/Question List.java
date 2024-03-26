866018a202aa8982054f40f32364274786efd90f
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2024-03-26 19:06:02 +0530

    Create Question List.java

---
+++ Java-Operator/Question List.java
@@ -0,0 +1,7 @@
+Q1 - Write a program to add 8 to the number x and then divide it by 3. Now, the modulus of the quotient
+ is taken with 5 and then multiply the resultant value by 5. Display the final result.
+Q2 - Swap two numbers without the use of third variable.
+Q3 - Write a program to calculate the sum of the digits of a 3-digit number.
+Q4 - Assign values of variables 'a' and 'b' as 55 and 70 respectively and then check if i) both the conditions 'a < 50' and 'a < b' are true.
+ii) at least one of the conditions 'a < 50' or 'a < b' is true.
+Q5 - Find the total number of bits needed to be flipped to convert x to y.
