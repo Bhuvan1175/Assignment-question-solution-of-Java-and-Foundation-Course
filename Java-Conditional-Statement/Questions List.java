1d50b26cc8ea0c2a9aa99681a6a6fb0efb1231e0
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2024-03-26 21:53:08 +0530

    Create Questions List.java

---
+++ Java-Conditional-Statement/Questions List.java
@@ -0,0 +1,31 @@
+# Conditional Questions #
+---------------------IF ELSE----------------------------
+1) Even Or Odd
+2) Take positive Integer input and tell if it is even or odd.
+3) Take positive Integer input and tell if it isdivisible by 5 or not.
+4) Any year is input through the keyboard. Write a program to determine whether the     year is a leap year or not. (Considering leap year occurs after every 4 years).
+5) Take integer input and print the absolute value of that integer.
+6) If cost price and selling price of an item is input through the keyboard, write a program to determine whether the seller has made profit or incurred loss. Also determine how much profit he made or loss he incurred.
+7) Given the length and breadth of a rectangle, write a program to find whether the area of the rectangle is greater than its perimeter.
+8) Taking Input and check the given length and breadth is side of square or not.
+-----------------------ELSE IF-----------------------------
+9)  Write a program to calculate marks to grades . Follow the conversion rule as given below : 91-100 Excellent,80-90 Very Good ,70-80 Good,60-70 Can do better,50-60 Average,40-50 Below Average,<40 Fail.
+10) Take positive integer input and tell if it is a three digit number or not.
+11) take Input & tell if it is a 2 digit Number.
+12) Take positive integer input and tell if it is divisible by 5 and 3.
+13) WAP to input a number and tell if it is divisible by 5 but not divisible by 3.
+14) Take positive integer input and tell if it is divisible by 5 or 3.
+15) Take 3 positive integers input and print the greatest of them.
+16) If the ages of Ram, Shyam and Ajay are input through the keyboard, write a program to determine the youngest of the three.
+17) Take 3 numbers input and tell if they can be the sides of a triangle.
+18)WAP to find if a number is divisible by both 3 & 5.
+----------------------Nested If----------------------------
+19) Take positive integer input and tell if it is divisible by 5 or 3 but not divisible by 15.
+20) Take 3 positive integers input and print the greatest of them.
+----------------------Maths---------------------------------
+21) Given three points (x1, y1), (x2, y2) and (x3, y3), write a program to check if all the three points fall on one straight line.
+22) Write a Java program called RadixNumber that takes user input for a number and determines its radix (base). The program should identify whether the input number is binary, octal, hexadecimal, or decimal and print the corresponding radix along with the number's base. 
+----------------------Switch Statements---------------------
+23) Make a calcultaor program all things is given by end user using Switch Statement.
+24)Create a Java program called SwitchDay that prompts the user to enter a number corresponding to a day of the week (1 for Monday, 2 for Tuesday, and so on, up to 7 for Sunday). The program should then use a switch statement to display the name of the day based on the entered number
+25) Write a Java program called PrimeNumberChecker that prompts the user to enter a positive integer. The program should then use a conditional statement to determine and print whether the entered number is prime or not.
