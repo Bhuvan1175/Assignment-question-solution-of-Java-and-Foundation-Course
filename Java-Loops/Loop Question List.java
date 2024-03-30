00cbc73a8369518d6fe1811c7740657cec47a61c
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2024-03-30 23:43:47 +0530

    Create Loop Question List.java

---
+++ Java-Loops/Loop Question List.java
@@ -0,0 +1,10 @@
+Loop Questions
+---------------------While Loop & For loop-----------------------------
+1) Print first 10 Natural Number series.
+2) Print the Sum of the first n natural number , when n is the input.
+3) Print the numbers in desending order with couple of deifference from 10.
+4) Print the sum of the stream of integers in the input.
+------------------------Break Keyword------------------------
+5) Print the first multiple of 5 which is also a multiple of 7.
+-----------------------Continue Keyword----------------------
+6) Print all the value between 1 and 50 except for the multiples of 3.
