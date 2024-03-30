9b71b8e07a72cae16460fe494202acb775ee6e1a
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2024-03-30 23:59:23 +0530

    Create  Que 03 : Print the numbers in desending order with couple of difference from 10.java

---
+++ Java-Loops/Que 03 : Print the numbers in desending order with couple of difference from 10.java
@@ -0,0 +1,8 @@
+// Que 03 : Print the numbers in desending order with couple of deifference from 10.
+public class desendingOrderNumber {
+    public static void main(String[] args) {
+        for(int i=10;i>=1;i-=2){
+            System.out.println(i);
+        }
+    }
+}
