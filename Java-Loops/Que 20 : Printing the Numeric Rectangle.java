1dfdcb0af80efa0f8bdb945d2422eb0892ff9f06
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2024-03-31 17:26:10 +0530

    Create Que 20 : Printing the Numeric Rectangle.java

---
+++ Java-Loops/Que 20 : Printing the Numeric Rectangle.java
@@ -0,0 +1,26 @@
+//Que 20 : Printing the Numeric Rectangle.
+//                   1 2 3 4 5 6 7 
+//                   2 3 4 5 6 7 1
+//                   3 4 5 6 7 1 2
+//                   4 5 6 7 1 2 3
+//                   5 6 7 1 2 3 4
+//                   6 7 1 2 3 4 5
+//                   7 1 2 3 4 5 6
+import java.util.Scanner;
+public class numericRectangle {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print( "Enter the number of rows:");
+        int row=sc.nextInt();
+        for(int i=1;i<=row;i++){
+            for(int j=i;j<=row;j++){
+                System.out.print(j);
+            }
+            for(int k=1;k<=i-1;k++){
+                System.out.print(k);
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+}
