f16602f4c61f06c89194c6ef865a0e71c9d48b0d
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2024-03-31 15:13:08 +0530

    Create Que 11 : Factorial of Any Number.java

---
+++ Java-Loops/Que 11 : Factorial of Any Number.java
@@ -0,0 +1,17 @@
+// Que 11 : Factorial of Any Number.
+import java.util.Scanner;
+
+public class factorial {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Any Number :");
+        int num = sc.nextInt();
+        int fact = 1;
+        for (int i = 1; i <= num; i++) {
+            fact = fact * i;
+        }
+        System.out.println("Factorial of " + num + " is: " + fact);
+        sc.close();
+
+    }
+}
