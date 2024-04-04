6a6e01ad2d023dab952e60164b1f14019df30e66
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2024-04-04 15:22:38 +0530

    Create Que 01 : Create a class name 'PERSON' with a variable age.java

---
+++ Java - OOPS/Que 01 : Create a class name 'PERSON' with a variable age.java
@@ -0,0 +1,8 @@
+// Que 01 : Create a class name 'PERSON' with a variable age.
+public class person{
+    int age=10;
+    public static void main(String[] args) {
+        person Rohan=new person();
+        System.out.println("Rohan Age : "+Rohan.age);
+    }
+}
