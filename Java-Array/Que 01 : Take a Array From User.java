747f2d718e661701530dabe3c048d82ac0db6d8c
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2024-04-06 01:33:24 +0530

    Create Que 01 : Take a Array From User.java

---
+++ Java-Array/Que 01 : Take a Array From User.java
@@ -0,0 +1,18 @@
+// Que 01 : Take a Array From User.
+import java.util.Scanner;
+public class userArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the Size of an Array");
+        int Size=sc.nextInt();
+        int A[]=new int[Size];
+        for(int i=0;i<Size;i++){
+            System.out.println("Enter the Value for "+(i+1)+" Index Element");
+            A[i]=sc.nextInt();
+        }
+        for(int i=0;i<Size;i++){
+            System.out.print(A[i]+" ");
+        }
+        sc.close();
+    }
+}
