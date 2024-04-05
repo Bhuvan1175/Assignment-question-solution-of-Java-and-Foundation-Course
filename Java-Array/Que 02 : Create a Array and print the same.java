37cedf837af68c2f4eafd9d72a720e462ae55b4a
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2024-04-06 01:37:32 +0530

    Create Que 02 : Create a Array and print the same.java

---
+++ Java-Array/Que 02 : Create a Array and print the same.java
@@ -0,0 +1,10 @@
+// Que 02 : Create a Array and print the same.
+public class createArray {
+    public static void main(String[] args) {
+        // int[] array = new int[5];
+        int Arr[]={1,2,3,4,5,7};
+        for(int i=0;i<Arr.length;i++){
+            System.out.print(Arr[i]+" ");
+        }
+    }
+}
