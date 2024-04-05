84f47042e2f544e9f0702fbae2487b9fdec6faa0
Bhuvaneshwar Bagde <142175031+Bhuvan1175@users.noreply.github.com>
2024-04-06 01:54:19 +0530

    Create Que 03 : Create Array from user and print them using For Each Loop.java

---
+++ Java-Array/Que 03 : Create Array from user and print them using For Each Loop.java
@@ -0,0 +1,20 @@
+// Que 03 : Create Array from user and print them using For Each Loop.
+
+import java.util.Scanner;
+
+public class forEach {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the Size of Array:");
+        int Size=sc.nextInt();
+        int A[]=new int[Size];
+        for(int i=0;i<Size;i++){
+            System.out.println("Enter the "+(i+1)+" Element of Array:");
+            A[i]=sc.nextInt();
+        }
+        for(int X:A){
+            System.out.print(X+" ");
+        }
+        sc.close();
+    }
+}
